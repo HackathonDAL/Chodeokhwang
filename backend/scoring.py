6b6a8c2fbcd607a487bb50afca124feb32541001
natacho0811 <chohyukeun05@gmail.com>
2026-09-12 13:54:29 +0900

    Update scoring.py

--- backend/scoring.py
+++ backend/scoring.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+# Leer los archivos CSV
+courses_df = pd.read_csv("data/courses.csv")
+fields_df = pd.read_csv("data/course_field_matrix.csv")
+
+# Mostrar los datos
+print("COURSES:")
+print(courses_df.head())
+
+print("\nFIELD MATRIX:")
+print(fields_df.head())
